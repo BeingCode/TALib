f905636d28fb36981012acc15d15fe49eca4fbea
next <xiang.zhang@51ysscw.com>
2021-07-24 11:25:28 +0800

    初始提交

---
+++ TALib/TALib-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef TALib_Bridging_Header_h
+#define TALib_Bridging_Header_h
+
+#import "ta_libc.h"
+
+#endif /* TALib_Bridging_Header_h */
---
+++ TALib/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TALib
+//
+//  Created by next on 2021/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+    
+}
+
